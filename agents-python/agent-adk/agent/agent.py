b209b01237e19bf0188a53332993d581c035f35a
lhs1012 <lhs1012@midasone.ai>
2025-11-04 08:10:21 +0900

    agent: add a function tool example.

--- agents-python/agent-adk/agent/agent.py
+++ agents-python/agent-adk/agent/agent.py
@@ -1,12 +1,29 @@
 from pathlib import Path
 from google.adk.models.lite_llm import LiteLlm
 from google.adk.agents.llm_agent import Agent
+from google.adk.tools import FunctionTool
 from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
 from google.adk.tools.mcp_tool.mcp_session_manager import StdioConnectionParams, SseConnectionParams
 from mcp import StdioServerParameters
 
 model_name = "openai/gpt-5-mini"
 llm = LiteLlm(model=model_name, timeout=180, stream_options={"include_usage": True})
+
+
+def get_weather_report(city: str) -> dict:
+    """Retrieves the current weather report for a specified city.
+
+    Returns:
+        dict: A dictionary containing the weather information with a 'status' key ('success' or 'error') and a 'report' key with the weather details if successful, or an 'error_message' if an error occurred.
+    """
+    if city.lower() == "london":
+        return {"status": "success", "report": "The current weather in London is cloudy with a temperature of 18 degrees Celsius and a chance of rain."}
+    elif city.lower() == "paris":
+        return {"status": "success", "report": "The weather in Paris is sunny with a temperature of 25 degrees Celsius."}
+    else:
+        return {"status": "error", "error_message": f"Weather information for '{city}' is not available."}
+
+weather_tool = FunctionTool(func=get_weather_report)
 
 def create_filesystem_toolset():
     workspace_dir = str((Path(__file__).parent / "workspace").absolute())
@@ -16,6 +33,15 @@ def create_filesystem_toolset():
             command='npx',
             args=["-y", "@modelcontextprotocol/server-filesystem", workspace_dir]
         )),
+        tool_filter=[]  # Optional: filter specific tools
+    )
+    return toolset
+
+def create_mcp_sse_server_toolset(url: str):
+    toolset = MCPToolset(
+        connection_params=SseConnectionParams(
+            url=url 
+        ),
         tool_filter=[]  # Optional: filter specific tools
     )
     return toolset
